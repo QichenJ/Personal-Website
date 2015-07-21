e3ae33d3e15e04d486fc3bc887998c9cde78a6f3
QichenJ <QichenJ@JingQichens-MacBook-Pro.local>
2015-07-20 23:06:47 -0400

    use js to show nav bar

---
+++ css/helper.js
@@ -0,0 +1,24 @@
+$(function() {
+	var nar = $('#nav');
+	var navHomeY = nav.offset().top;
+	var isFixed = false;
+	var $w = $(window);
+	$w.scroll(function () {
+		var scrollTop = $w.scrollTop();
+		var shouldBeFixed = scrollTop > navHomeY;
+		if(shouldBeFixed && !isFixed) {
+			nav.css({
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: nav.width()
+			});
+			isFixed = true;
+		}else if (!shouldBeFixed && isFixed) {
+			nav.css({
+				position: 'static'
+			});
+			isFixed = false;
+		}
+	});
+});
