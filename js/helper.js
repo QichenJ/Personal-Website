08f228f20ba07c2b04485ed0c5431dfe193dc997
QichenJ <QichenJ@JingQichens-MacBook-Pro.local>
2015-07-30 14:14:54 -0400

    add highlight for nav bar

--- js/helper.js
+++ js/helper.js
@@ -1,10 +1,33 @@
-
-
 (function(){
+
+	//This function is tp help smotthscrolling
 	$('a[href^="#"]').on('click', function(){
 		$('body').animate({
 			scrollTop: $(this.hash).offset().top}, 1000);
 		return false;
 	});
 
+	//Help to change color when in its section
+	$(window).on('scroll resize', onScreen);
+
 })();
+
+
+
+function onScreen() {
+	$('.section').each(function() {
+		var windowScroll = $(document).scrollTop();
+		var navHeight = $('nav ul').height();
+		var $this = $(this)
+
+		//check if the navbar is in this section
+
+		if((windowScroll + navHeight >= $this.offset().top) &&
+			(windowScroll + navHeight <= $this.offset().top + $this.height())) {
+			$('li a#nav-' + $this.attr('id')).addClass('highlight');
+		console.log($('li a#nav-' + $this.attr('id')));
+		} else {
+			$('li a#nav-' + $this.attr('id')).removeClass('highlight');
+		}
+	});
+}
